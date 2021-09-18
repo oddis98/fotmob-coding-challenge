50a6c23ea42afc44f3428e489c3f332185e0f796
Oddbjørn <47176972+oddis98@users.noreply.github.com>
2021-09-18 13:27:45 +0200

    Delete index.test.ts

--- index.test.ts
+++
@@ -1,6 +0,0 @@
-import { it, expect } from "@jest/globals";
-import { includeExclude, range } from "./index";
-
-it("should return an array of numbers", () => {
-  expect(range(5, 10)).toEqual([5, 6, 7, 8, 9, 10]);
-});
